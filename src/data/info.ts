2b9675399b8b11aab146ba2da6d2295faf40db09
Dhia M <merzougui.dhia@gmail.com>
2024-11-04 08:47:42 +0100

    Updated portfolio

--- src/data/info.ts
+++ src/data/info.ts
@@ -1,97 +1,111 @@
 export const info = {
-  baseUrl: "https://astro-portfolio-uzair.vercel.app",
-  name: "Uzair Ahmed",
-  jobDescription: "Web Developer",
-  about: `I'm a software engineer based in Karachi, Pakistan.
-   I enjoy creating things that live on the internet, whether that be websites, applications, or anything in between.
-   I have been working for 3 year, with a demonstrated history of working in the Computer Software industry. Skilled in Front-end Development.
-   I have 3 years of experience in HTML, CSS, SCSS, JavaScript, Bootstrap, tailwind CSS, StyledComponent, React JS and Next JS. Hence,
-   I have successfully completed several web projects from scratch. 
-   Yet, I learn continuously and never stop exploring new technologies.
-   So, I have also gained knowledge of Node JS, Express JS, Mongo DB, Python,
-   firebase and React Native with time.
+  baseUrl: "https://www.dhia.dev",
+  name: "Dhia Merzougui",
+  jobDescription: "Software Engineer",
+  about: `I'm a software engineer with a dual focus in Fullstack Development and AI R&D, currently developing patient-monitoring and analysis tools in the healthcare sector at GREYC Laboratory.
+  My fascination with software development ignited at a young age, and that passion has only grown stronger over the years. I love building things end-to-end, ranging from web, mobile, and desktop apps, to machine learning/AI models.
+  Beyond my professional interests, I'm a geek through and through, passionate about all things tech. Off-screen, I enjoy rock climbing, diving into a good fiction book, and exploring different cultures.
+  I'm always open to new opportunities for growth, collaboration, and contribution. Don’t hesitate to reach out!
   `,
 
   experience: [
     {
-      name: "InterNative Labs, LLC",
-      location: "Karāchi, Sindh, Pakistan",
-      startDate: "Apr 2022",
+      name: "Software Engineer - GREYC Laboratory",
+      location: "Caen, Normandy, France",
+      startDate: "Sep 2024",
       endDate: "Present",
       description: [
-        "- Developing screens and UI components for the web application using React and Tailwind.",
-        "- Fixing UI issues and integrating backend APIs with Redux Saga.",
+        "✓ End-to-end design, implementation, and testing of new feature for the patient-monitoring platform A2MIMO in: React/Javascript, Python/FastAPI, Unity/C#, PostgreSQL, Docker",
+        "✓ Writing and updating technical documentation.",
+        "✓ Refactoring old legacy code.",
+        "✓ Helping with purchase decisions for hardware for psycho-analysis room currently being constructed.",
+        "✓ Development of a web app for the automatic generation and correction of clinical notes using generative AI (LLMs) for healthcare professionals.",
       ],
     },
 
     {
-      name: "Coupon Approve (Remote)",
-      location: "Amsterdam, North Holland, Netherlands",
-      startDate: "Nov 2020",
-      endDate: "Apr 2022",
+      name: "Software Engineering Intenr - GREYC Laboratory",
+      location: "Caen, Normandy, France",
+      startDate: "Feb 2024",
+      endDate: "Aug 2024",
       description: [
-        "✓ Developing code in accordance with appropriate design and architectural rules",
-        "✓ Documenting, testing, deployment, maintenance, support of platforms, applications, databases and solutions",
-        "✓ Help and training of other colleagues",
-        "✓ Others tasks connected to the nature of the job",
-        "✓ Code Improvements and Refactoring ",
-        "✓ Best Practices and Rules Maintainer",
+        "✓ Developed user interfaces for visualization of real-time patient analysis data.",
+        "✓ Developed backend APIs for real-time collection, processing, analysis, and storage of multi-modal patient data (text, audio, images).",
+        "✓ Developed neural classifiers for monitoring patient status (fatigue, emotions, sentiments) based on text, images, and audio data.",
+        "✓ Developed an LLM-based conversation engine for the patient-facing virtual agent interface.",
+        "✓ Developed the deployment strategy using Docker and Docker Compose.",
+        "✓ Carried out research on automatic generation of clinical notes for psychiatry using LLMs.",
+        "✓ Co-wrote research paper on fake news and hyperpartisanship detection using LLMs.",
+        "✓ Supervised and mentored junior intern.",
       ],
     },
 
     {
-      name: "Interns Pakistan",
-      location: "Pakistan",
-      startDate: "Oct 2020",
-      endDate: "Nov 2020",
+      name: "Student Developer - GREYC Laboratory",
+      location: "Caen, Normandy, France",
+      startDate: "Oct 2022",
+      endDate: "Jan 2024",
       description: [
-        "An Immersive Internship Program that prepares me for the world's most in-demand career. A six weeks program completely based on learning and challenges.",
-        "The internship track is built by an industry expert that let develop the skills in the areas most important ",
+        "Assisted a team of researchers and engineers in prototyping software to monitor and analyze mental health patients.",
+        "✓ Developed user interfaces for data visualization.",
+        "✓ Developed a prototype of a conversational agent using GPT-3.",
       ],
     },
   ],
 
   education: [
     {
-      name: "Pakistan Institute of Professional Science & Technology (PIPS-TECH)",
-      location: "Karāchi, Sindh, Pakistan",
-      startDate: "2019",
-      endDate: "2022",
+      name: "University of Caen Normandy",
+      location: "Caen, Normandy, France",
+      startDate: "2022",
+      endDate: "2024",
       description: [
-        "Diploma of Associate Engineering,  Computer Information Technology (CIT)",
+        "Master's in Computer Science: Algorithms and Intelligent Systems",
+        "Graduated with honors",
       ],
     },
     {
-      name: "Govt. Degree Boys College Buffer Zone",
-      location: "Karāchi, Sindh, Pakistan",
+      name: "University of Caen Normandy",
+      location: "Caen, Normandy, France",
       startDate: "2018",
-      endDate: "2020",
-      description: ["Intermediate, Pre-Engineering"],
-    },
-    {
-      name: "Ghazi Foundation Schools & Colleges",
-      location: "Karāchi, Sindh, Pakistan",
-      startDate: "2016",
-      endDate: "2018",
-      description: ["Matriculation,  Computer Science"],
+      endDate: "2022",
+      description: ["Bachelor's in Computer Science"],
     },
   ],
 
   socialMedia: {
-    facebook: "https://www.facebook.com/profile.php?id=100022899849583",
-    twitter: "https://twitter.com/Uzzii_21",
-    github: "https://github.com/uzzii-21",
-    email: "mailto:uzairahmed@74372gmail.com",
-    linkedin: "https://www.linkedin.com/in/uzzii21/",
+    github: "https://github.com/HikariLight",
+    email: "mailto:merzougui.dhia@gmail.com",
+    linkedin: "https://www.linkedin.com/in/dhia-merzougui/",
   },
 
   projects: [
     {
-      title: "Astro Portfolio",
+      title: "A2MIMO: Artificial Agent for Mind Monitoring",
       isFeatured: true,
-      thumbnail: "/assets/images/astro-portfolio.png",
-      githubUrl: "https://github.com/uzzii-21/astro-portfolio",
-      liveUrl: "https://astro-portfolio-uzair.vercel.app/",
+      thumbnail: "/assets/images/A2MIMO.svg",
+      liveUrl: "https://www.youtube.com/watch?v=6rCiBtaXeb8",
+    },
+    {
+      title: "KeyAuth",
+      isFeatured: true,
+      thumbnail: "/assets/images/KeyAuth.svg",
+      githubUrl: "https://github.com/HikariLight/KeyboardAuth",
+      liveUrl: "https://keyauth.dhia.dev",
+    },
+    {
+      title: "Briefer",
+      isFeatured: true,
+      thumbnail: "/assets/images/briefer.png",
+      githubUrl: "https://github.com/HikariLight/Briefer",
+      liveUrl: "https://briefer.dhia.dev",
+    },
+    {
+      title: "Meal Planner",
+      isFeatured: true,
+      thumbnail: "/assets/images/meal-planner.svg",
+      githubUrl: "https://github.com/HikariLight/Meal-Planner",
+      liveUrl: "https://meal-planner.dhia.dev",
     },
   ],
 };
